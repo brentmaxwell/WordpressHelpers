bcdf9a4704deaaf4e158c4408bda7720b88c117c
Brent Maxwell <brent@thebrent.net>
2015-04-06 13:57:42 -0400

    added save functionality for custom post type metaboxes and geometabox

--- CustomPostType.php
+++ CustomPostType.php
@@ -7,6 +7,7 @@ class CustomPostType{
 		add_action( 'add_meta_boxes_'.$this->post_type, array( $this, 'add_meta_box' ) );
 		add_filter('manage_edit-'.$this->post_type.'_columns', array($this,'add_columns'));
 		add_action('manage_'.$this->post_type.'_posts_custom_column', array($this,'display_columns'), 10, 2);
+		add_action( 'save_post', array( $this, 'save_metabox' ) );
 	}
 	
 	public function activate(){
@@ -40,25 +41,64 @@ class CustomPostType{
 	public function render_meta_box($post,$metabox){
 		echo "<table>";
 		echo "<tbody>";
-		
 		// Add an nonce field so we can check for it later.
-		wp_nonce_field( $args['id'].'_metabox', $args['id'].'_metabox_nonce' );
+		wp_nonce_field( $metabox['args']['id'].'_metabox', $metabox['args']['id'].'_metabox_nonce' );
 
 		$meta = get_post_meta($post->ID);
 		foreach($metabox['args']['fields'] as $field){
-			echo $this->generate_metabox_field($field['id'],$meta[$field['id']][0],$field['title']);	
+			echo $this->generate_metabox_field($field['id'],$meta[$field['id']][0],$field['title'],$field['type']);	
 		}
 		echo "</tbody>";
 		echo "</table>";
 	}
 	
-	public function generate_metabox_field($meta_id,$meta_value,$label){
-		$output =  '<tr><td><label for="'.$meta_id.'">'.
-		__( $label, 'myplugin_textdomain' ).
-		'</label></td><td>'.
-		'<input type="text" id="'.$meta_id.'" name="'.$meta_id.'"'.
-        ' value="' . esc_attr( $meta_value ) . '" size="25" /></td></tr>';
+	public function generate_metabox_field($meta_id,$meta_value,$label,$type="text"){
+		$output =  '<tr><td><label for="'.$meta_id.'">';
+		$output .=  __( $label);
+		$output .= '</label></td><td>';
+		$output .= '<input type="'.$type.'" id="'.$meta_id.'" name="'.$meta_id.'"';
+		switch($type)
+		{
+			case 'checkbox':
+				$output .= ' value="1" '.checked( $meta_value, true, false );
+				break;
+			default:
+				$output .= ' value="'.esc_attr( $meta_value ).'"';
+				break;
+		}
+		$output .= '"/></td></tr>';
+        
 		return $output;
+	}
+	
+	public function save_metabox($post_id){
+		foreach($this->meta_boxes as $metabox){
+			if ( ! isset( $_POST[$metabox['id'].'_metabox_nonce'] ) ) {
+			return;
+			}
+		
+			if ( ! wp_verify_nonce( $_POST[$metabox['id'].'_metabox_nonce'], $metabox['id'].'_metabox' ) ) {
+				return;
+			}
+		
+			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+				return;
+			}
+			
+			if ( 'page' == $_POST['post_type'] ) {
+				if ( ! current_user_can( 'edit_page', $post_id ) )
+					return $post_id;
+			} else {
+				if ( ! current_user_can( 'edit_post', $post_id ) )
+					return $post_id;
+			}
+	
+			foreach($metabox['fields'] as $field){
+				$field_data = sanitize_text_field( $_POST[$field['id']] );
+				update_post_meta( $post_id, $field['id'], $field_data );	
+			}
+		}
+		
 	}
 	
 	public function add_columns($columns){
@@ -69,8 +109,10 @@ class CustomPostType{
 	}
 	
 	function display_columns($column_name, $id) {
-		if(array_key_exists($column_name,$this->columns)){
-			echo get_post_meta($id,$column_name,true);
+		if(isset($this->columns)){
+			if(array_key_exists($column_name,$this->columns)){
+				echo get_post_meta($id,$column_name,true);
+			}
 		}
 	}   
 }
--- GeoMetabox.php
+++ GeoMetabox.php
@@ -1,4 +1,5 @@
 <?php
+
 class GeoMetaBox{
 	public function __construct(){
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
@@ -18,13 +19,11 @@ class GeoMetaBox{
 		echo "<table>";
 		echo "<tbody>";
 		
-		
-		// Add an nonce field so we can check for it later.
 		wp_nonce_field( 'geo_metabox', 'geo_metabox_nonce' );
 
 		$meta = get_post_meta($post->ID);
 		echo "<tr>";
-		echo $this->generate_metabox_field('geo_public',$meta['geo_public'][0],'Public');
+		echo $this->generate_metabox_field('geo_public',$meta['geo_public'][0],'Public','checkbox');
 		echo '</tr>';
 		echo "<tr>";
 		echo $this->generate_metabox_field('geo_latitude',$meta['geo_latitude'][0],'Latitude');
@@ -40,28 +39,64 @@ class GeoMetaBox{
 			array(
 				'height' => 254,
 				'width' => 254
-			)
-		);
+			));
 		echo '<img src="'.$mapsUrl.'"/>';
 	}
 	
-	public function generate_metabox_field($meta_id,$meta_value,$label){
-		$output =  '<td><label for="'.$meta_id.'">'.
-		__( $label, 'myplugin_textdomain' ).
-		'</label></td><td>'.
-		'<input type="text" id="'.$meta_id.'" name="'.$meta_id.'"'.
-        ' value="' . esc_attr( $meta_value ) . '"/></td>';
+	public function generate_metabox_field($meta_id,$meta_value,$label,$type="text"){
+		$output =  '<td><label for="'.$meta_id.'">';
+		$output .=  __( $label);
+		$output .= '</label></td><td>';
+		$output .= '<input type="'.$type.'" id="'.$meta_id.'" name="'.$meta_id.'"';
+		switch($type)
+		{
+			case 'checkbox':
+				$output .= ' value="1" '.checked( $meta_value, true, false );
+				break;
+			default:
+				$output .= ' value="'.esc_attr( $meta_value ).'"';
+				break;
+		}
+		$output .= '"/></td>';
+        
 		return $output;
+	}
+	
+	public function save_metabox($post_id){
+		if ( ! isset( $_POST['geo_metabox_nonce'] ) ) {
+			return;
+		}
+	
+		if ( ! wp_verify_nonce( $_POST['geo_metabox_nonce'], 'geo_metabox' ) ) {
+			return;
+		}
+	
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+		
+		if ( 'page' == $_POST['post_type'] ) {
+			if ( ! current_user_can( 'edit_page', $post_id ) )
+				return $post_id;
+		} else {
+			if ( ! current_user_can( 'edit_post', $post_id ) )
+				return $post_id;
+		}
+
+		$geo_latitude = sanitize_text_field( $_POST['geo_latitude'] );
+		$geo_longitude = sanitize_text_field( $_POST['geo_longitude'] );
+		$geo_public = $_POST['geo_public'];
+		update_post_meta( $post_id, 'geo_latitude', $latitude );
+		update_post_meta( $post_id, 'geo_longitude', $geo_longitude );
+		update_post_meta( $post_id, 'geo_public', $geo_public );
 	}
 	
 	public function mapsUrl($location,$size){
 		$baseUrl = 'https://maps.google.com/maps/api/staticmap?maptype=terrain';
 		$baseUrl .= '&amp;size='.$size['width'].'x'.$size['height'];
 		$baseUrl .= '&amp;sensor=false';
-//		$baseUrl .= '&amp;center='.$location['lat'].','.$location['lng'];
 		$baseUrl .= '&amp;markers=color:red%7C'.$location['lat'].','.$location['lng'];
 		return $baseUrl;
 	}
 }
-
 $geoMetaBox = new GeoMetaBox();
