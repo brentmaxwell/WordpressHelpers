2fdd04b7e8f88d50898094c7f3bb8cc839a5592e
Brent Maxwell <brent@thebrent.net>
2015-04-29 12:51:33 -0400

    Added base plugin file

---
+++ helpers.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Helpers
+Description: Helpers
+Author: Brent Maxwell
+Version: 0.1
+*/
+
+include('CustomTaxonomy.php');
+include('CustomPostType.php');
+include('Geo.php');
